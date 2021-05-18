d0756e6cd32f5aa584aa403d457eb0ba25bf4cd3
archaemedes <wadecroft@gmail.com>
2021-05-18 02:12:11 +0000

    Wrote some event handling logic for when backspace key was held down to update the count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,12 +3,24 @@ $(document).ready(() => {
 
   const counter = document.getElementById("counter");
 
+
+
   $("#tweet-text").keyup(function(event) {
     const elementName = this.name;
     const tweetBox = $(`textarea#${elementName}`).val();
-    const charCount = tweetBox.length;
-
-    counter.innerHTML = 140 - tweetBox.length;
+    const charCount = 140 - tweetBox.length;
+    if (charCount < 0) {
+      counter.style.color = "red";
+    }
+    counter.innerHTML = charCount;
     console.log(charCount);
+    console.log(event.which);
   });
+
+  $("#tweet-text").keydown(function(event) {
+    const elementName = this.name;
+    const tweetBox = $(`textarea#${elementName}`).val();
+    const charCount = 140 - tweetBox.length;
+    counter.innerHTML = charCount;
+  })
 });
