5f9c79257dcfdc1ec4a21f977ec8a0b8b5fcf8f0
archaemedes <wadecroft@gmail.com>
2021-05-19 03:34:09 +0000

    Refactored code to use classes instead of IDs

--- public/scripts/tweet-card.js
+++ public/scripts/tweet-card.js
@@ -1,27 +1,27 @@
 // This script uses only JQuery DOM manipulation to practice its use.
 $(document).ready(() => {
   // Creates shadow of tweet on hover
-  $(".tweet-feed").hover(function(){
+  $(".post").hover(function(){
     $(this).css("box-shadow", "5px 8px #888888");
   }, function(){
     $(this).css("box-shadow", "");
   });
   // Changes the color of the icons when hovered with mouse
-  $("#retweet").hover(function(){
+  $(".retweet").hover(function(){
     $(this).css("color", "rgb(253, 197, 124)");
   }, function(){
     $(this).css("color", "");
   });
-  $("#like").hover(function(){
+  $(".like").hover(function(){
     $(this).css("color", "rgb(253, 197, 124)");
   }, function(){
     $(this).css("color", "");
   });
-  $("#flag").hover(function(){
+  $(".flag").hover(function(){
     $(this).css("color", "rgb(253, 197, 124)");
   }, function(){
     $(this).css("color", "");
   });
   // Show the time since the tweet was made
-  $("#date").html(timeago.format(1621204295892));
+  $(".date").html(timeago.format(1621204295892));
 });
